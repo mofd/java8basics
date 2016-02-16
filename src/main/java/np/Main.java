78b13a9a13361c3bd8b32bece00829c597b5956f
mofd <konstantin.steuer@email.de>
2016-02-16 17:25:19 +0100

    initial

---
+++ src/main/java/np/Main.java
@@ -0,0 +1,10 @@
+package np;
+
+/**
+ * TODO (KS) Dokumentieren.
+ *
+ * @author steuer.konstantin <br>
+ * copyright (C) 2016, SWM Services GmbH
+ */
+public class Main {
+}
---
+++ src/test/java/np/MainTest.java
@@ -0,0 +1,14 @@
+package np;
+
+
+import org.junit.Test;
+
+/**
+ * TODO (KS) Dokumentieren.
+ *
+ * @author steuer.konstantin <br>
+ * copyright (C) 2016, SWM Services GmbH
+ */
+public class MainTest {
+
+}
